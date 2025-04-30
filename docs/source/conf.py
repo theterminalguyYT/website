474b5c1f2c0efddaeb8900a4c47eb78f02ffc478
TheTerminalGuy <theterminalguy.yt@gmail.com>
2025-04-29 17:57:39 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'TheTerminalGuy'
+copyright = '2025, TheTerminalGuy'
+author = 'TheTerminalGuy'
 
 release = '0.1'
 version = '0.1.0'
